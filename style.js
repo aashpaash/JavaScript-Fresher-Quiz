79d33296d1a69a80d7e6ebc53637293bd3508ee3
A P <ashleighpaschal@gmail.com>
2022-11-12 22:06:11 -0600

    style.css

---
+++ style.js
@@ -0,0 +1,10 @@
+//QUESTIONS
+var questions = [
+    {
+        question: "How much does a roof cost?",
+        choices: ["a. nothing it's on the house", "b. the house", "c. house and home"],
+        answer: "a. nothing it's on the house"
+    },
+    {
+        question: "Why are elevator jokes so classic and good?",
+        choices:
